feaadda30b02fb1ce45ebdf8125941d9fbd9e35f
TheHadiAhmadi <thehadiahmadi@gmail.com>
2024-05-13 11:37:14 +0430

    Update text

--- src/lib/Form/Image.js
+++ src/lib/Form/Image.js
@@ -16,17 +16,17 @@ export default component({
                     <div class="transition top-0 start-0 bg-transparent group-hover:bg-black/50 flex w-full h-full justify-center items-center gap-2 absolute">
                         <button x-show="!value" @click="onFileUpload" type="button" class="opacity-0 group-hover:opacity-100 flex items-center justify-center gap-2 text-white bg-teal-700 hover:bg-teal-800 focus:ring-4 focus:ring-teal-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-teal-600 dark:hover:bg-teal-700 focus:outline-none dark:focus:ring-teal-800">
                             ${Icon({name: 'upload'})}
-                            ${props.uploadText}
+                            ${props.uploadText ?? 'Upload'}
                         </button>
                         <button @click="onFileRemove" type="button" class="opacity-0 group-hover:opacity-100 flex items-center justify-center gap-2 text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800">
                             ${Icon({name: 'times'})}
-                            ${props.removeText}
+                            ${props.removeText ?? 'Remove'}
                         </button>
                     </div>
                 </div>
                 <div x-show="!preview && !value" class="flex absolute h-full w-full items-center flex-col justify-center">
                     <div class="relative flex items-center justify-center text-white bg-teal-700 hover:bg-teal-800 focus:ring-4 focus:ring-teal-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-teal-600 dark:hover:bg-teal-700 focus:outline-none dark:focus:ring-teal-800">
-                        <span>${props.chooseText}</span>
+                        <span>${props.chooseText ?? 'Choose'}</span>
                         <input @change="onFileChange" type="file" class="absolute top-0 bottom-0 opacity-0 left-0 right-0">
                     </div>
                 </div>
